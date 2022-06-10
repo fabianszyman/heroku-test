d10727087da062d3c31c8e0b13311393e6efac90
Fabian Szyman <fabianszyman@gmail.com>
2022-06-10 15:30:03 +0200

    adding playwright-chromium

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ const express = require('express');
 const app = express();
 const port = 1000
 
+const { chromium } = require("playwright-chromium");
+
 // express Settings
 app.get('/', function (req, res) {
     console.log('This is a Console Log');
+    (async () => {
+        const browser = await chromium.launch({ chromiumSandbox: false });
+        const context = await browser.newContext();
+        const page = await context.newPage();
+        await page.goto('http://whatsmyuseragent.org/');
+        console.log(' chromium Started');
+        await browser.close();
+        console.log(' chromium Ended');
+      })();
     res.send('Handelsregister-Scraper started!')
 })
 
