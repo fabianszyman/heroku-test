d2fed25f24e0ef8596061d09d1fafdac44deefc9
Fabian Szyman <fabianszyman@gmail.com>
2022-06-10 16:30:33 +0200

    add puppeteer-core

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 1000
-
-const { puppeteer } = require("puppeteer");
+const puppeteer = require('puppeteer');
 
 // express Settings
 app.get('/', function (req, res) {
@@ -12,12 +11,13 @@ app.get('/', function (req, res) {
             headless: true,
             args: ['--no-sandbox','--disable-setuid-sandbox']
         })
-        const context = await browser.newContext();
-        const page = await context.newPage();
+        const page = await browser.newPage();
         await page.goto('http://whatsmyuseragent.org/');
         console.log(' chromium Started');
         await browser.close();
         console.log(' chromium Ended');
+      
+        await browser.close();
       })();
     res.send('Handelsregister-Scraper started!')
 })
