f49ed52f5430f9d7ca7ee50850aff438b8e92068
Fabian Szyman <fabianszyman@gmail.com>
2022-06-10 15:17:51 +0200

    installing express package

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const port = 1000
 
 // express Settings
 app.get('/', function (req, res) {
@@ -8,5 +9,5 @@ app.get('/', function (req, res) {
 })
 
 app.listen(process.env.PORT || 1000, '0.0.0.0', () => {
-    console.log("Handelsregister Scraper can be started on Port 4000.");
+    console.log(`Example app listening on port ${port}`)
 })
